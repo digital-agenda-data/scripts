0407e62c90a6493a8bdc23c786a82d81dcd3378a
Andrei Melis <andrei.melis@eaudeweb.ro>
2018-02-28 17:17:13 +0200

    Rebase before push

--- export-codelists/exportcl.sh
+++ export-codelists/exportcl.sh
@@ -10,4 +10,9 @@ do
 	curl --data-urlencode "format=application/x-nice-turtle" --data-urlencode "query=${QUERY}" http://digital-agenda-data.eu/sparql > dad-${dimension}.ttl
 done
 
-git add -u && git commit -m "Codelist updates" && git push
+dimension='time-period'
+QUERY="construct {?s ?p ?o} where { graph ?g { ?s ?p ?o } filter (?g in (<http://semantic.digital-agenda-data.eu/codelist/time-period/>, <http://semantic.digital-agenda-data.eu/codelist/time-period>)) }"
+curl --data-urlencode "format=text/plain" --data-urlencode "query=${QUERY}" http://digital-agenda-data.eu/sparql | LC_ALL=C sort -n > dad-${dimension}.nt
+curl --data-urlencode "format=application/x-nice-turtle" --data-urlencode "query=${QUERY}" http://digital-agenda-data.eu/sparql > dad-${dimension}.ttl
+
+git add -u && git commit -m "Codelist updates" && git pull --rebase && git push
