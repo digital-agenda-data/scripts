2ae6ade54bbe4088c037e42db12e9ce1f2d5b3f3
Andrei Melis <andrei.melis@eaudeweb.ro>
2019-01-29 10:10:09 +0200

    Updated script for python 3

--- check_whitelist/check_whitelist.py
+++ check_whitelist/check_whitelist.py
@@ -1,10 +1,12 @@
-WHITELIST_URL = 'http://test.digital-agenda-data.eu/datasets/digital_agenda_scoreboard_key_indicators/whitelist.json'
-#WHITELIST_URL = 'http://localhost:8440/Plone/datasets/digital_agenda_scoreboard_key_indicators/whitelist.json'
-SPARQL_ENDPOINT = 'http://test-virtuoso.digital-agenda-data.eu/sparql'
+import json
+from urllib.parse import urlencode
+from urllib.request import urlopen
 
 from string import Template
-import json
-import urllib, urllib2
+
+WHITELIST_URL = 'http://digital-agenda-data.eu/datasets/digital_agenda_scoreboard_key_indicators/whitelist.json'
+#WHITELIST_URL = 'http://localhost:8440/Plone/datasets/digital_agenda_scoreboard_key_indicators/whitelist.json'
+SPARQL_ENDPOINT = 'http://virtuoso.digital-agenda-data.eu/sparql'
 
 
 with open('query-obs.sparql', 'r') as file:
@@ -13,18 +15,18 @@ with open('query-obs.sparql', 'r') as file:
 with open('query-ind-group.sparql', 'r') as file:
     template_ind_group = Template(file.read())
 
-whitelist = json.loads(urllib2.urlopen(WHITELIST_URL).read())
+whitelist = json.loads(urlopen(WHITELIST_URL).read())
 for row in whitelist:
     params = {}
     # query indicator groups
     params['query'] = template_ind_group.substitute(indicator=row['indicator'].lower())
     params['format'] = 'application/sparql-results+json'
-    url = SPARQL_ENDPOINT + "?" + urllib.urlencode(params)
-    result = json.loads(urllib2.urlopen(url).read())
+    url = SPARQL_ENDPOINT + "?" + urlencode(params)
+    result = json.loads(urlopen(url).read())
     ind_group = result['results']['bindings'][0]['groupnotation']['value']
     if row['indicator-group'].lower() != ind_group.lower():
-        print "Different indicator group for " + row['indicator'] + " actual: " + ind_group
-    
+        print("Different indicator group for " + row['indicator'] + " actual: " + ind_group)
+
     # query observations
     params['query'] = template_obs.substitute(
         indicator=row['indicator'].lower(), 
@@ -32,9 +34,9 @@ for row in whitelist:
         unit=row['unit-measure'].lower()
     )
     params['format'] = 'application/sparql-results+json'
-    url = SPARQL_ENDPOINT + "?" + urllib.urlencode(params)
+    url = SPARQL_ENDPOINT + "?" + urlencode(params)
 
-    result = json.loads(urllib2.urlopen(url).read())
+    result = json.loads(urlopen(url).read())
     count = int(result['results']['bindings'][0]['callret-0']['value'])
-    if ( count == 0 ):
-        print row
+    if (count == 0):
+        print("No observations for: " + str(row))
